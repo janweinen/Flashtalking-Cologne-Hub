b7db3e787cf6f21587214f369511d00decb2994a
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2022-02-01 11:50:08 +0100

    update to returnNameFromEmail function because it broke with new name pattern

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import { DataProvider } from "./components/Context";
 import "./styles.css";
 
 const App = () => {
-  const returnNameFromEmail = email => {
+  const returnNameFromEmail = (email) => {
     if (typeof email !== "string") return "";
     const rawName = email.split("@");
-    const names = rawName[0].split(".");
-    const forname = names[0].charAt(0).toUpperCase() + names[0].slice(1);
-    const surname = names[1].charAt(0).toUpperCase() + names[1].slice(1);
-    return forname + " " + surname;
+    //const names = rawName[0].split(".");
+    //const forname = names[0].charAt(0).toUpperCase() + names[0].slice(1);
+    //const surname = names[1].charAt(0).toUpperCase() + names[1].slice(1);
+    //return forname + " " + surname;
+    return rawName[0];
   };
   const [data, setData] = useState({ signedIn: false });
   useEffect(() => {
     let collection = {};
     try {
-      auth.onAuthStateChanged(user => {
+      auth.onAuthStateChanged((user) => {
         if (user) {
           collection = {
             signedIn: true,
